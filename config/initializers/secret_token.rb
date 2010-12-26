95fed459953fb4c7c3bf7fdbfb9f27da06f526b9
Felipe Urbina <furbina@furbina-MacBookPro.local>
2010-12-26 00:01:05 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corramos::Application.config.secret_token = '9888c951868b40e1d7fb024f7ee140cb13dba694aedb121c8f364b4ed42c6b7595689c01c883c2efc02c63a57b26ccfeefb8b56bc1ab0928ed3eb211b6265a6b'
